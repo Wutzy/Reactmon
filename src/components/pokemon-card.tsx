a21490b6789493c68e9626be52be98c0b4aa4f52
Wutzy <matmati.anis@gmail.com>
2023-07-14 06:48:03 +0200

    adding navigation with hook useHistory

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -3,6 +3,7 @@ import Pokemon from '../models/pokemon';
 import './pokemon-card.css';
 import formatDate from '../helpers/format-date';
 import formatType from '../helpers/format-type';
+import { useHistory } from 'react-router-dom';
   
 type Props = {
   pokemon: Pokemon
@@ -12,16 +13,22 @@ type Props = {
 const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'}) => {
   
   const [color, setColor] = useState<string>();  
+  const history = useHistory();
 
   const showBorder = () => {
     setColor(borderColor);
   }
+
   const hideBorder = () => {
     setColor('#f5f5f5'); // on remets la bordure initial : grise.
   }
 
+  const goToPokemon = (id: number) => {
+    history.push(`/pokemons/${id}`);
+  }
+
   return (
-    <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+    <div className="col s6 m4" onClick={() => goToPokemon(pokemon.id)} onMouseEnter={showBorder} onMouseLeave={hideBorder}>
       <div className="card horizontal" style={{ borderColor: color }}>
         <div className="card-image"> 
           <img src={pokemon.picture} alt={pokemon.name}/>
