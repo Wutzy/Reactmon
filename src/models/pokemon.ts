cef1bdfc9c6b00f88297ff72b4191c5998509509
Wutzy <matmati.anis@gmail.com>
2023-10-27 14:57:14 +0200

    refacto pokemon model

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -1,24 +1,16 @@
 export default class Pokemon {
-    // 1. Typage des propiétés d'un pokémon.
-    id: number;
-    hp: number;
-    cp: number;
-    name: string;
-    picture: string;
-    types: Array<string>;
-    created?: Date;
-     
-    // 2. Définition des valeurs par défaut des propriétés d'un pokémon.
+
+    // 1. Définition des valeurs par défaut des propriétés d'un pokémon.
     constructor(
-     id: number,
-     hp: number = 100,
-     cp: number = 10,
-     name: string = '...',
-     picture: string = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/XXX.png',
-     types: Array<string> = ['Normal'],
-     created: Date = new Date()
+        public id: number,
+        public hp: number = 100,
+        public cp: number = 10,
+        public name: string = '...',
+        public picture: string = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/XXX.png',
+        public types: Array<string> = ['Normal'],
+        public created: Date = new Date()
     ) {
-     // 3. Initialisation des propiétés d'un pokémons.
+     // 2. Initialisation des propiétés d'un pokémons.
      this.id = id;
      this.hp = hp;
      this.cp = cp;
